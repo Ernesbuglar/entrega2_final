13bbe0dce8be4b47988cc3c6549e303ffbc97eec
Ernesbuglar <ernesto_5197@escuelaces.net>
2024-05-20 14:29:39 +0200

    kufchfchj

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <header class="flex justify-between mx-10 my-10">
+         <div>logo</div>
+
+         <div class="flex gap-7">
+            <div class="pesto"><button><NuxtLink to="/">home</NuxtLink></button></div>
+            <div class="pesto"><button><NuxtLink to="/about">about</NuxtLink></button></div>
+            <div class="pesto"><button><NuxtLink to="/products">products</NuxtLink></button></div>
+            
+         </div>
+    >
+</header>
+    </div>
+    <div>
+        <slot 7></slot>
+    </div>
+</template>
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -3,11 +3,16 @@
       <h1 class="text-3xl font-bold mx-10 my-10 tex text-lime-600">
       Productos
     </h1>
+    <div class="grid grid-cols-4 gap-5 mx-10">
+
+    </div>
+    <div v-for="p in products"></div>
+    <nuxt-link :to="products/${p.id}">{{ p.tittle }}</nuxt-link>
     </div>
   </template>
 
 <script setup>
-
+const {data:products} = await useFetch ('https://fakestoreapi.com/products')
 </script>
 
 <style lang="scss" scoped>
